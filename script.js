858fa0a4d8fcd50e08cc58dc59e2854326b3046e
RickPijnenburg <32143619+RickPijnenburg@users.noreply.github.com>
2021-09-17 17:27:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = "Rick";
+console.log(name);
+let num1 = 4 + 4;
+let num2 = "4" + "4";
+console.log(num1);
+console.log(num2);
+let num3 = 3 + 3;
+let num4 = 4 - 3;
+let num5 = 3 * 3;
+let num6 = 3 / 3;
+let num7 = 25 % 3;
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+let age = "45";
+console.log(typeof(age));
